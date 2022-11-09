166c65f4e90482cc84e4fa114d688f8c1e9330fe
bishbro <biswas.rai102@gmail.com>
2022-11-09 12:30:53 +0545

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+/* eslint-disable comma-dangle */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'standard-with-typescript',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    'no-console': 'error',
+    '@typescript-eslint/semi': 'off',
+    'jsdoc/check-tag-names': 'off',
+    'func-names': 'off',
+    'require-jsdoc': [
+      'error',
+      {
+        require: {
+          ArrowFunctionExpression: true,
+        },
+      },
+    ],
+  },
+};
